2edf63bafae22ad87dfca7286cd870f5e88a7a43
Lukas Råberg <lukas.raberg@inuse.se>
2018-05-23 08:33:56 +0200

    Styling on page

--- page.php
+++ page.php
@@ -1,18 +1,20 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header class="header">
-<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-</header>
-<section class="entry-content">
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-<?php the_content(); ?>
-<div class="entry-links"><?php wp_link_pages(); ?></div>
-</section>
-</article>
-<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
-<?php endwhile; endif; ?>
-</section>
-<?php get_sidebar(); ?>
+	<section id="content" role="main" class="fullpage">
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		
+		<div class="page-header">
+			<h1 class="mono center xxl"><?php the_title(); ?></h1> 
+		</div>
+		
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<section class="page-container">
+				<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+				<p class="serif m regular"><?php the_content(); ?></p>
+				<div class="entry-links"><?php wp_link_pages(); ?></div>
+			</section>
+		</article>
+		<?php edit_post_link(); ?>
+<!--	<?php if ( ! post_password_required() ) comments_template( '', true ); ?> -->
+	<?php endwhile; endif; ?>
+	</section>
 <?php get_footer(); ?>
